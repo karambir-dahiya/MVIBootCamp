2ec1951c7e5597d1aaa11613ca9da156e6e688de
Karambir <karmvirdahiya.com@gmail.com>
2026-06-06 12:28:07 +0530

    Initial Setup

---
+++ MVIBootCamp/MVIBootCamp/MVIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIBootCampApp.swift
+//  MVIBootCamp
+//
+//  Created by Siyaa Dahiya on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MVIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
